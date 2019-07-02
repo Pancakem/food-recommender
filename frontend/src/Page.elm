4bac88d34adfc5e394f6462c684159decdabe397
pancakem <pancakesdeath@protonmail.com>
2019-07-02 20:59:44 +0300

    moving elm files to frontend

--- frontend/src/Page.elm
+++ frontend/src/Page.elm
@@ -4,10 +4,8 @@ import Browser exposing (Document)
 import Html exposing (..)
 
 type Page = 
-    Home
-    | Blog
-    | Other
-    | SearchResult
+    Landing
+    | Home
 
 view : Page -> { title : String, content : Html msg } -> Document msg
 view page { title, content } =
