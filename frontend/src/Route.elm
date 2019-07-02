be0d7235010cf63320e0edc4c278c5e4a9368911
Pancakem <pancakesdeath@protonmail.com>
2019-07-03 00:06:13 +0300

    add login and register routing

--- frontend/src/Route.elm
+++ frontend/src/Route.elm
@@ -7,11 +7,16 @@ import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
 type Route = 
     Landing
     | Home
+    | Register
+    | Login
 
 parser : Parser (Route -> a) a
 parser = 
     oneOf
         [ Parser.map Landing Parser.top
+        , Parser.map Register (s "register")
+        , Parser.map Login (s "login")
+        , Parser.map Home (s "home")
         ]
 
 fromUrl : Url -> Maybe Route 
@@ -33,5 +38,11 @@ toPath route =
 
                 Home ->
                     ["home"]
+                
+                Login -> 
+                    ["login"]
+                
+                Register -> 
+                    ["register"]
     in
     String.join "/" path
