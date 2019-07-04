0d3964d6b7514658871436b8f47fcd1c1f209079
Pancakem <pancakesdeath@protonmail.com>
2019-07-04 15:36:51 +0300

    redirect to login if not logged in

--- frontend/src/Page/Home.elm
+++ frontend/src/Page/Home.elm
@@ -1,18 +1,58 @@
-module Page.Home exposing (..)
+module Page.Home exposing (Model, Msg, update, view, subscriptions, init)
 
-import Session exposing (Session)
+import Html exposing (..)
+import Session exposing (Session, cred)
+import Route
 
 init : Session -> (Model, Cmd msg)
 init session =
+    let
+        cmd =
+            case cred session of
+                Just cred ->
+                    Route.replaceUrl (Session.navKey session) Route.Home
+
+                Nothing ->
+                    Cmd.none
+    in
+    
     ({
         session = session
     }
-    , Cmd.none
+    , cmd
      )
         
 type alias Model = 
     {session : Session}
 
+type Msg
+    = Msg1
+    | Msg2
 
--- type Msg = 
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Msg1 ->
+            (model, Cmd.none)
+
+        Msg2 ->
+            (model, Cmd.none)
+
+
+view : Model -> {title : String, content : Html Msg}
+view model =
+    {title = "Home"
+    , content = 
+        div []
+            [ text "New Html Program" ]
+    }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+toSession : Model -> Session
+toSession {session} = 
+    session
 
