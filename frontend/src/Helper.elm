bbc97e200104fa8a0c569fbb3384a1fc2c0039a4
Pancakem <pancakesdeath@protonmail.com>
2019-07-09 19:37:50 +0300

    add helper functions

--- frontend/src/Helper.elm
+++ frontend/src/Helper.elm
@@ -1,21 +1,47 @@
-module Helper exposing (endPoint, ErrorMessage(..), asString)
+module Helper exposing (endPoint, Response, decodeResponse, informHttpError)
 
-import RemoteData exposing (..)
-import Session exposing (Session)
+import User exposing (Profile)
+import Json.Decode exposing (field, Decoder, string, map3, map2)
+import Json.Encode as Encode
 import Http
-
-type alias Model = 
-    { session : Session }
-
 
 endPoint : String 
 endPoint = 
     ""
 
-type ErrorMessage = 
-    ErrorMessage String
+type alias Response =
+    { token : String
+    , profile : Profile
+    }
 
-                
-asString : ErrorMessage -> String
-asString (ErrorMessage str) =
-    str
+decodeResponse : Decoder Response
+decodeResponse = 
+    map2 Response
+        (field "token" string)
+        (field "profile" decodeProfile)
+
+decodeProfile : Decoder Profile
+decodeProfile = 
+    map3 Profile
+        (field "fullname" string)
+        (field "email" string)
+        (field "id" string)
+
+informHttpError : Http.Error -> String
+informHttpError err = 
+    case err of 
+        Http.BadStatus _ ->
+            "Oh the request failed. Something must've gone wrong. Try again."
+        
+        Http.Timeout ->
+            "Server took too long to respond."
+        
+        Http.NetworkError ->
+            "Network error. Please check your wifi."
+        
+        Http.BadBody _ ->
+            "Oooh something went wrong. Try again!"   
+        
+        Http.BadUrl _ ->
+            "Invalid url."
+                            
