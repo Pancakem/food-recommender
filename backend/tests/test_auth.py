72d6616cde1e4c5bd0b2370587fa4540a437b5b1
Pancakem <pancakesdeath@protonmail.com>
2019-07-15 09:46:39 +0300

    test authentication

---
+++ backend/tests/test_auth.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+
+from ..main import db
+from ..main.model import User
+from project.tests.base import BaseTestCase
+
+
+class TestAuthBlueprint(BaseTestCase):
+    def test_registration(self):
+    """ Test for user registration """
+    with self.client:
+        response = self.client.post(
+            '/auth/register',
+            data=json.dumps(dict(
+                email='malea@gmail.com',
+                password='123456'
+            )),
+            content_type='application/json'
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'success')
+        self.assertTrue(data['message'] == 'Successfully registered.')
+        self.assertTrue(data['auth_token'])
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 201)
+    
+    def test_registered_with_already_registered_user(self):
+    """ Test registration with already registered email"""
+    user = User(
+        email='malea@gmail.com',
+        password='test'
+    )
+    db.session.add(user)
+    db.session.commit()
+    with self.client:
+        response = self.client.post(
+            '/auth/register',
+            data=json.dumps(dict(
+                email='malea@gmail.com',
+                password='123456'
+            )),
+            content_type='application/json'
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(
+            data['message'] == 'User already exists. Please Log in.')
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 202)
+
+
+if __name__ == '__main__':
+    unittest.main()
