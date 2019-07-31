a928517a26502501c36394181e9c5bb41c5c3f06
pancakem <pancakesdeath@protonmail.com>
2019-07-31 07:58:54 +0300

    implement logout

--- frontend/src/Page/Home.elm
+++ frontend/src/Page/Home.elm
@@ -52,6 +52,7 @@ type Msg
     = NavbarMsg Navbar.State
     | GetRecommendation
     | GotRecommendation  (Result Http.Error Recommendation)
+    | LoggedOut
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -75,6 +76,9 @@ update msg model =
             
             (newModel, Cmd.none)
 
+        LoggedOut ->
+            (model, Cmd.batch [Session.logout, Route.pushUrl (Session.navKey model.session) Route.Login ])
+
 
 view : Model -> {title : String, content : Html Msg}
 view model =
@@ -84,6 +88,9 @@ view model =
             [ viewNavbar model
             , p [class "validation-problem"]
                 (List.map (\str -> viewServerError str) model.problem)
+            , h3 [style "text-align" "center"] [text "Did You Know!"]
+            , viewDidyouknow
+            , br [] []
             , viewRecommendation model
             ]
     }
@@ -99,19 +106,24 @@ viewNavbar model =
         |> Navbar.brand [ class "nav-menu-logo", href "/" ] [ text "EatRight" ]
         |> Navbar.items
             [ Navbar.itemLink [ class "navbar-dropdown nav-links nav-menu", href "/settings" ] [ text "Settings" ]
-            , Navbar.itemLink [ class "navbar-dropdown nav-links nav-menu", href "/logout" ] [ text "Logout" ]
+            , Navbar.itemLink [ class "navbar-dropdown nav-links nav-menu", onClick LoggedOut ] [ text "Logout" ]
             ]
         |> Navbar.view model.navbarState
 
 viewRecommendation : Model -> Html Msg
 viewRecommendation model =
-    div [ style "text-align" "center"]
+    div [ style "text-align" "center" ]
         [
-        div [] 
+        div [style "color" "green"] 
             [ text model.recommendation.food ]
         , br [] []
         , div []
             [ button [ class "recommend-button", onClick GetRecommendation ] [ text "Get Recommendation" ]]
+        ]
+
+viewDidyouknow = 
+    div [style "display" "block"]
+        [ p [] [text (List.head tips |> Maybe.withDefault "")]
         ]
 
 
@@ -145,3 +157,20 @@ decodeRecommendation =
     Decode.map Recommendation
         (Decode.field "food" Decode.string)
 
+
+tips : List String
+tips = 
+    ["""Don’t drink sugar calories. Sugary drinks are among the most fattening items you can put into your body. 
+    This is because your brain doesn’t measure calories from liquid sugar the same way it does for solid food.
+    Therefore, when you drink soda, you end up eating more total calories."""
+    , """Eat nuts. Despite being high in fat, nuts are incredibly nutritious and healthy. 
+        They’re loaded with magnesium, vitamin E, fiber, and various other nutrients.
+        Studies demonstrate that nuts can help you lose weight and may help fight type 2 diabetes and heart disease. 
+        Additionally, your body doesn’t absorb 10–15% of the calories in nuts. 
+        Some evidence also suggests that this food can boost metabolism. 
+        In one study, almonds were shown to increase weight loss by 62%, compared with complex carbs."""
+    , """Avoid processed junk food (eat real food instead). Processed junk food is incredibly unhealthy.
+        These foods have been engineered to trigger your pleasure centers, so they trick your brain into overeating — even promoting food addiction in some people.
+        They’re usually low in fiber, protein, and micronutrients but high in unhealthy ingredients like added sugar and refined grains. 
+        Thus, they provide mostly empty calories."""
+    ]
